af7eb420c5b2a08815adbb0fede2120ccc0399a3
sariumit935-cpu <sariumit935@gmail.com>
2026-05-14 18:22:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const chromium = require('@sparticuz/chromium');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/generate', async (req, res) => {
-  const { html } = req.body;
+  const html = req.body.html;
+  
+  console.log('Received HTML:', html ? html.substring(0, 100) : 'EMPTY');
+  
+  if (!html) {
+    return res.status(400).json({ error: 'HTML is empty' });
+  }
   
   try {
     const browser = await puppeteer.launch({
@@ -23,6 +30,7 @@ app.post('/generate', async (req, res) => {
     res.set('Content-Type', 'image/png');
     res.send(screenshot);
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: err.message });
   }
 });
